43f2e6fd5876a23de9069654b48387ddc524b529
Ahmed <ahmed_l2@yahoo.com>
2024-07-19 01:13:39 +0300

    0. Log parsing

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -7,8 +7,9 @@ import sys
 def stats():
     """reads stdin line by line and computes metrics"""
     file_size = 0
-    status_codes = {}
     counter = 0
+    status_codes = {200: 0, 301: 0, 400: 0, 401: 0, 403: 0, 404: 0,
+                    405: 0, 500: 0}
 
     try:
         for line in sys.stdin:
@@ -19,29 +20,29 @@ def stats():
             )
             match = re.search(pattern, line)
             if match:
-                counter += 1
-                scode = match.group(3)
-                fsize = match.group(4)
+                scode = int(match.group(3))
+                fsize = int(match.group(4))
                 if scode in status_codes:
                     status_codes[scode] += 1
-                else:
-                    status_codes[scode] = 1
-                file_size += int(fsize)
+                file_size += fsize
 
+            counter += 1
             if counter >= 10:
                 print_stats(file_size, status_codes)
                 counter = 0
 
     except KeyboardInterrupt:
+        pass
+    finally:
         print_stats(file_size, status_codes)
-        raise
 
 
 def print_stats(file_size, status_codes):
     """Prints the statistics"""
     print("File size: {}".format(file_size))
     for code in sorted(status_codes.keys()):
-        print("{}: {}".format(code, status_codes[code]))
+        if status_codes[code] > 0:
+            print(f"{code}: {status_codes[code]}")
 
 
 if __name__ == "__main__":
